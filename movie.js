2774c8082a4f4f75549591557db790a721bdeea8
Amgloria <106662052+Amgloria@users.noreply.github.com>
2022-06-30 08:50:42 +0000

    my mobie book

--- movie.js
+++ movie.js
@@ -7,7 +7,7 @@ const movieError = document.querySelector('.movie-error')
 const urlError =document.querySelector('.url-error')
 const rateError =document.querySelector('.rate-error')
 const numberError = document.querySelector('.number-error')
-const movieTitle = document.querySelector('.movTitle')
+const movieTitle = document.querySelector('.movTitle') 
 const imageUrl = document.querySelector('.imageUrl')
 const rating = document.querySelector('.rating')
 
@@ -23,24 +23,28 @@ function addMovieModal(){
 addMovie.addEventListener('click',addMovieModal);
 
 baseCancel.addEventListener('clicK',function(){
-    document.querySelector(".div1").style.display="none";
+    backdrop.classList.add('hidden');
+    document.getElementById("div1").style.display="none";
 })
 
 backdrop.addEventListener('click', function(){
     backdrop.classList.add('hidden');
     document.getElementById("div1").style.display="none";
 })
+let id=1
 
 function saveMovie(e){
     e.preventDefault()
     
-    const movieTitle =  document.getElementById("movie").value;
-    console.log(movieTitle);
+
+    
+    const titleOfMovie =  document.getElementById("movie").value;
+    console.log(titleOfMovie);
     const image = document.getElementById("image").value;
     console.log(image);
     const rate =document.getElementById("rate").value;
     console.log(rate);
-    if (!movieTitle){
+    if (!titleOfMovie){
         movieError.classList.remove('hidden')
         return;
     }
@@ -59,15 +63,51 @@ function saveMovie(e){
         return;
     }
 
-    const movie = {title:movieTitle, url:image, rating:rate}
+    const movie = {id:id,title:titleOfMovie, url:image, rating:rate}
+    
     movies.push(movie)
+ 
     console.log(movies);
+    const movieList = document.querySelector('.movie-list')
+
+    const newMovie =document.createElement('div')
+    newMovie.classList.add('new-movie')
+
+    const movieId =document.createElement('span')
+    movieId.textContent=id;
+    movieId.classList.add('movie-id')
+    newMovie.append(movieId)
+
+    const newMovieTitle =document.createElement('span')
+    newMovieTitle.textContent = titleOfMovie;
+    newMovie.append(newMovieTitle)
+
+    const movieImage = document.createElement('img')
+    movieImage.src = image
+    movieImage.classList.add('movie-image')
+    newMovie.append(movieImage)
+
+    const ratin =document.createElement('span')
+    ratin.textContent = rate
+    ratin.classList.add('rat-in')
+    newMovie.append(ratin)
+
+
+    movieList.appendChild(newMovie)
+    id++
+
+    document.getElementById("movie").value = '';
+    document.getElementById("image").value = '';
+    document.getElementById("rate").value = '';
    
+    backdrop.classList.add('hidden');
+    document.getElementById("div1").style.display="none";
     
  }
 
 add.addEventListener('click',saveMovie)
 
- 
-document.body.appendChild(newElement);
+
+
+
 
